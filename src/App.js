766f25943f98d66ed03e6c8bea76df66810a0825
peculiar okon <peculiargirl4real@gmail.com>
2025-05-22 20:54:44 +0100

    about scratched

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/home";
 import Login from "./pages/auth/login";
 import Navbar from "./components/layout/navbar";
+import About from './components/layout/about';
 
+//   // 1. Create Theme Context
+// export const ThemeContext = createContext();
 function App() {
+
+  // const [theme, setTheme] = useState('light'); // default theme
+
+  // const toggleTheme = () => {
+  //   setTheme(prev => (prev === 'light' ? 'dark' : 'light'));
+  
   return (
     <Router>
+         <About />
+
       <Routes>
         <Route
           path="/"
@@ -20,8 +31,22 @@ function App() {
 
         <Route path="/login" element={<Login />} />
       </Routes>
-    </Router>
-  );
-}
+    </Router>)
+};
+
+//      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+//       <div className={theme === 'dark' ? 'dark-mode' : 'light-mode'}>
+//         <Router>
+//           <Navbar /> {/* Has the toggle button */}
+//           <About />   {/* This stays globally if modal */}
+//           <Routes>
+//             <Route path="/" element={<Homepage />} />
+//             <Route path="/login" element={<Login />} />
+//           </Routes>
+//         </Router>
+//       </div>
+//     </ThemeContext.Provider>
+//   );
+// }
 
 export default App;
